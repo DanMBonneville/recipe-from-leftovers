53caca89a0e84b6e6d2cc382abca5e6cb66f3c94
Dan Bonneville <DanMBonneville@gmail.com>
2024-03-12 15:01:16 -0500

    Create mini back for safe usage of api key

---
+++ src/store/actions/actions.tsx
@@ -0,0 +1,14 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const createGetRecipesURL = (ingredients: String[]) => {
+  let url = 'http://localhost:8000/getRecipesFromIngredients';
+  return (url += `?ingredients=${ingredients.join()}`);
+};
+
+export const getRecipes = createAsyncThunk(
+  'getRecipesFromIngredients',
+  async (ingredients: String[]) => {
+    return await axios.get(createGetRecipesURL(ingredients));
+  }
+);
