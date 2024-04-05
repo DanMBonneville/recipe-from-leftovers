8c964764cbde414dc15335864f2209326aa9844e
Dan Bonneville <DanMBonneville@gmail.com>
2024-04-04 21:14:36 -0500

    fix styling in prod

--- src/components/SearchPageComponents/SelectSubmitIngredients/SelectBar.tsx
+++ src/components/SearchPageComponents/SelectSubmitIngredients/SelectBar.tsx
@@ -1,12 +1,34 @@
+import React from 'react';
 import Select from 'react-select';
 import { SelectBarProps } from '../../../common/types';
 
 const SelectBar = (props: SelectBarProps) => {
   const { isSelectDisabled, options, handleSelectionChange } = props;
 
+  const isMobile = window.matchMedia('(max-width: 768px)').matches;
+
+  const customStyles = {
+    control: (provided: any) => ({
+      ...provided,
+      width: '100%',
+      overflow: 'hidden',
+    }),
+    container: (provided: any) => ({
+      ...provided,
+      width: isMobile ? '88%' : '56%',
+    }),
+    placeholder: (provided: any) => ({
+      ...provided,
+      display: 'inline-block',
+      whiteSpace: 'nowrap',
+      fontSize: isMobile ? 'calc(100vw / 33)' : '20px',
+    }),
+  };
+
   return (
     <Select
       aria-label="Select Ingredients"
+      styles={customStyles}
       options={options}
       value={null}
       placeholder={"What's in your fridge?"}
