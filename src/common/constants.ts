ddbc59da1faa7cb4d2a086d2f1e07fdaad11771f
Dan Bonneville <DanMBonneville@gmail.com>
2024-04-15 11:13:33 -0500

    add constants

---
+++ src/common/constants.ts
@@ -0,0 +1,14 @@
+// Login Errors
+export const INVALID_EMAIL = 'INVALID_EMAIL';
+export const INVALID_LOGIN_CREDENTIALS = 'INVALID_LOGIN_CREDENTIALS';
+export const MISSING_PASSWORD = 'MISSING_PASSWORD';
+
+// Login Error Messages
+export const INVALID_EMAIL_MSG = 'Please enter a valid email address.';
+export const INVALID_LOGIN_CREDENTIALS_MSG =
+  'Incorrect email or password. Please try again.';
+export const MISSING_PASSWORD_MSG = 'Please enter a password.';
+
+// Sign up Errors
+
+// Singup Error Messages
