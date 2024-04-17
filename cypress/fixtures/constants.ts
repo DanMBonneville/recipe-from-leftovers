0f718bbc9808b05794093d3be73cfba7f6b0da4a
Dan Bonneville <DanMBonneville@gmail.com>
2024-04-17 09:45:32 -0500

    refactor login e2e

--- cypress/fixtures/constants.ts
+++ cypress/fixtures/constants.ts
@@ -5,3 +5,11 @@ export const stableDomDefaultParams = {
   pollInterval: pollDefault,
   timeout: timoutDefault,
 };
+
+export const testUser1Email = 'test@test.com';
+export const testUser2Password = '123456';
+
+export const loginUri = '/login';
+export const searchPageUri = '/search-for-recipes';
+export const searchresultsPageUri = '/recipe-preview-list';
+export const recepeDetailsPageUri = '/recipe/details';
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -12,6 +12,15 @@ Cypress.Commands.add('clearSessionGoToRoot', () => {
   });
 });
 
+Cypress.Commands.add('login', (email: string, password: string) => {
+  cy.clearSessionGoToRoot();
+  cy.findByTestId('login-email-input').clear();
+  cy.findByTestId('login-password-input').clear();
+  if (email) cy.findByTestId('login-email-input').type(email);
+  if (password) cy.findByTestId('login-password-input').type(password);
+  cy.findByTestId('login-button').click();
+});
+
 Cypress.Commands.add('deleteUserByUid', (uid: string) => {
   return cy.wrap(axios.post('/api/fire/delete-user', { uid }));
 });
@@ -20,6 +29,7 @@ declare global {
   namespace Cypress {
     interface Chainable {
       clearSessionGoToRoot(): Chainable<void>;
+      login(email: string, password: string): Chainable<void>;
       deleteUserByUid(uid: string): Chainable<AxiosResponse<any>>;
     }
   }
